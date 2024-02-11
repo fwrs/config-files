75934009a86d9cb1c67d858fa07bc498dd14c0e5
Ilya <ilyusha_k@icloud.com>
2024-02-12 00:24:56 +0300

    Create xcode-nvim.sh

---
+++ xcode-nvim.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# I bind this to ⌃⌘,
+osascript -e "
+tell application \"Xcode\"
+    set x to path of last item of source documents
+end tell
+
+tell application \"iTerm\"
+    activate
+    tell current window
+        create tab with default profile
+        delay 0.3
+        tell current session
+            write text \"nvim \" & x
+        end tell
+    end tell
+end tell"
