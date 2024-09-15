f1424cda6f2f4411c7d97eda4cb42bee3ea836df
Ilya <ilyafwrs@gmail.com>
2024-09-16 00:27:58 +0300

    Update steam-totp.py

--- steam-totp.py
+++ steam-totp.py
@@ -6,7 +6,7 @@ from time import time
 from urllib.parse import urlparse, parse_qs
 from subprocess import check_output
 
-url = check_output(["op", "read", "op://Personal/Steam/one-time password"])
+url = check_output(["op", "read", "op://Personal/Steam/totp secret"])
 query = urlparse(url).query
 key_b32 = parse_qs(query)[b"secret"][0]
 key = b32decode(key_b32)
