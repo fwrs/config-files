b690d8c5b32c6b36dd34f4bc76ff9ab5005f5ad0
Ilya <ilyafwrs@gmail.com>
2024-10-11 08:53:19 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -14,13 +14,6 @@ set -gx HOMEBREW_NO_AUTO_UPDATE 1
 set -gx HOMEBREW_NO_ENV_HINTS 1
 set -gx GPG_TTY (tty)
 
-fish_vi_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
-function fish_mode_prompt; end
-
 abbr -a gl "git log --graph --all --oneline --decorate --pretty=format:\"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\""
 abbr -a gfixup "git commit -a --amend --no-edit"
 abbr -a gsw "git switch"
