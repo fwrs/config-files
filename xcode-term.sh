6db50ca0c9f233e6d11b6d56ab5502958925231d
Ilya <ilyafwrs@gmail.com>
2024-10-12 03:51:19 +0300

    Update xcode-term.sh

--- xcode-term.sh
+++ xcode-term.sh
@@ -4,6 +4,11 @@ dir="$PWD"
 # Remove a potential suffix in case Xcode shows a Swift Package
 suffix="/.swiftpm/xcode"
 dir=${dir//$suffix/}
-echo "#!/bin/sh\n/opt/homebrew/bin/fish -C \"cd \\\"$dir\\\"\"" > ~/.xcode/launch_term.command
+
+echo "
+#!/bin/sh
+export fish_startup_cwd=$dir
+/opt/homebrew/bin/fish -i" > ~/.xcode/launch_term.command
+
 chmod +x ~/.xcode/launch_term.command
 open ~/.xcode/launch_term.command
