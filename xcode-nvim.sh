fef89f7b6f090c23d2c05536b9cb85d4281119a7
Ilya Kulinkovich <ilyafwrs@gmail.com>
2026-05-02 01:03:38 +0200

    Update xcode-nvim.sh

--- xcode-nvim.sh
+++ xcode-nvim.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 # I bind this to ⌃⌘,
+
 path=$(osascript -e "
 tell application \"Xcode\"
     set x to path of last item of source documents
 end tell
-
 return x")
 
-dir="$PWD"
-suffix="/.swiftpm/xcode"
-dir=${dir//$suffix/}
-path=${path#"$dir/"}
+dir=$(printf '%s' "$PWD" | sed 's|/.swiftpm/xcode||')
+path="${path#"$dir/"}"
+esc_dir=$(printf '%s' "$dir" | sed 's/\\/\\\\/g; s/"/\\"/g')
+esc_path=$(printf '%s' "$path" | sed 's/\\/\\\\/g; s/"/\\"/g')
 
-echo "
-#!/bin/sh
-export fish_startup_cwd=\"$dir\"
-export fish_startup_command=\"nvim \\\"$path\\\"\"
-/opt/homebrew/bin/fish -i" > ~/.xcode/launch_nvim.command
-
-chmod +x ~/.xcode/launch_nvim.command
-open ~/.xcode/launch_nvim.command
+osascript \
+    -e "tell application \"Ghostty\"" \
+    -e "    set cfg to new surface configuration" \
+    -e "    set initial working directory of cfg to \"$esc_dir\"" \
+    -e "    set win to new window with configuration cfg" \
+    -e "    set term to focused terminal of selected tab of win" \
+    -e "    input text \"nvim \\\"$esc_path\\\"\" to term" \
+    -e "    send key \"enter\" to term" \
+    -e "end tell"
